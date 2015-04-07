ca9d322b17d37c601703d1a90f2de60b2ac574c8
Oytun <oytunozdemir@gmail.com>
2015-04-07 12:20:42 +0300

    + ‘Framework/Core/Mathematica.php’

---
+++ Framework/Core/Mathematica.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Mathematica class.
+ */
+class Mathematica{
+	/**
+	 * float function.
+	 * 
+	 * @access public
+	 * @param mixed $num
+	 * @return void
+	 */
+	function toFloat($num) {
+	    $dotPos = strrpos($num, '.');
+	    $commaPos = strrpos($num, ',');
+	    $sep = (($dotPos > $commaPos) && $dotPos) ? $dotPos : 
+	        ((($commaPos > $dotPos) && $commaPos) ? $commaPos : false);
+	   
+	    if (!$sep) {
+	        return floatval(preg_replace("/[^0-9]/", "", $num));
+	    } 
+	
+	    return floatval(
+	        preg_replace("/[^0-9]/", "", substr($num, 0, $sep)) . '.' .
+	        preg_replace("/[^0-9]/", "", substr($num, $sep+1, strlen($num)))
+	    );
+	}
+}
+?>
